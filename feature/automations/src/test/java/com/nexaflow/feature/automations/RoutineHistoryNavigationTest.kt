b7fa4718dca4b4b837abfac4667fcd94e512eeac
Alaa91H <1.43001551e+08+Alaa91H@users.noreply.github.com>
2026-08-26 23:48:29 +0000

    test: run routine history route under Robolectric

--- feature/automations/src/test/java/com/nexaflow/feature/automations/RoutineHistoryNavigationTest.kt
+++ feature/automations/src/test/java/com/nexaflow/feature/automations/RoutineHistoryNavigationTest.kt
@@ -2,7 +2,12 @@ package com.nexaflow.feature.automations
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class RoutineHistoryNavigationTest {
 
     @Test
