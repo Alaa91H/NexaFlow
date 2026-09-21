da8395735ab23de8a2c9bb278f4f45148d011526
Alaa <alahus2591@gmail.com>
2026-09-21 13:35:45 +0200

    Cover failed builder saves

--- feature/automation-builder/src/test/java/com/nexaflow/feature/builder/BuilderSaveCoordinatorTest.kt
+++ feature/automation-builder/src/test/java/com/nexaflow/feature/builder/BuilderSaveCoordinatorTest.kt
@@ -2,6 +2,7 @@ package com.nexaflow.feature.builder
 
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.yield
 import org.junit.Assert.assertFalse
@@ -9,6 +10,22 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class BuilderSaveCoordinatorTest {
+
+    @Test
+    fun postSaveFlow_doesNotRunAfterCancelledSave() = runBlocking {
+        val saveJob = launch {
+            error("simulated save failure")
+        }
+
+        var postSaveFlowRan = false
+        val postSaveJob = launchAfterSave(saveJob) {
+            postSaveFlowRan = true
+        }
+
+        postSaveJob.join()
+
+        assertFalse(postSaveFlowRan)
+    }
 
     @Test
     fun postSaveFlow_waitsForPersistenceJob() = runBlocking {
