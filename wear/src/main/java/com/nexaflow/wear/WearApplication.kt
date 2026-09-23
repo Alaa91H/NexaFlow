7b94198dac6daf8e8c46e579afe19f57db84301b
Alaa <alahus2591@gmail.com>
2026-09-23 22:54:24 +0200

    Publish watch capabilities at app startup

--- wear/src/main/java/com/nexaflow/wear/WearApplication.kt
+++ wear/src/main/java/com/nexaflow/wear/WearApplication.kt
@@ -1,10 +1,21 @@
 package com.nexaflow.wear
 
 import android.app.Application
+import com.nexaflow.wear.data.WearCapabilityPublisher
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 /**
  * Hilt application class for the Wear OS companion app.
  */
 @HiltAndroidApp
-class WearApplication : Application()
+class WearApplication : Application() {
+
+    @Inject
+    lateinit var capabilityPublisher: WearCapabilityPublisher
+
+    override fun onCreate() {
+        super.onCreate()
+        capabilityPublisher.start()
+    }
+}
